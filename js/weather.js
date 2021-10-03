bf32f51d4f2a7b5b6e0733c0bf360f3764eac509
unknown <81542046+Briley0205@users.noreply.github.com>
2021-10-03 10:46:29 +0900

    addweather

---
+++ js/weather.js
@@ -0,0 +1,18 @@
+const API_KEY = "0eca97ee3331005e1b06a557263d7634"
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    fetch(url).then(Response => Response.json()).then(data => {
+        const weatherContainer = document.querySelector("#weather span:last-child");
+        const cityContainer = document.querySelector("#weather span:first-child");
+        cityContainer.innerText = `${data.name} / `;
+        weatherContainer.innerText = ` ${data.weather[0].main} / ${data.main.temp}°`
+    });
+}
+function onGeoError() {
+    alert("Can't find you. No wether for you.")
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
